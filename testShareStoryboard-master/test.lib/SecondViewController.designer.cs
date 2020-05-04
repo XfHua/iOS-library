c3addef89325b186f6c2e19521283beaf2198a79
XfHua <jasonHuaxf@163.com>
2020-05-04 16:01:18 +0800

    Add files via upload

---
+++ testShareStoryboard-master/test.lib/SecondViewController.designer.cs
@@ -0,0 +1,23 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace test.lib
+{
+    [Register ("SecondViewController")]
+    partial class SecondViewController
+    {
+        [Outlet]
+        UIKit.UITextField SecondText { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
